926b169f9a0bdb42e1cd9002c484bf01267a66f1
lzmattana <l.z.mattana@gmail.com>
2023-04-20 14:28:12 -0300

    adicionando busca com parametros dinamicos

--- src/main/java/lzm_loja/dao/ProdutoDao.java
+++ src/main/java/lzm_loja/dao/ProdutoDao.java
@@ -1,9 +1,11 @@
 package lzm_loja.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import lzm_loja.modelo.Produto;
 
@@ -53,6 +55,31 @@ public class ProdutoDao {
 		return em.createQuery(jpql, BigDecimal.class).setParameter("nome", nome).getSingleResult(); // metodo para
 																									// retornar somente
 																									// um resultado
+
+	}
+
+	public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		String jpql = "SELECT p FROM Produto p WHERE 1=1 "; // gambiarra pra forçar o where na querry
+		if (nome != null && !nome.trim().isEmpty()) {
+			jpql = " AND p.nome = :nome ";
+		}
+		if (preco != null) {
+			jpql = " AND p.preco = :preco ";
+		}
+		if (dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro ";
+		}
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		if (nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", query);
+		}
+		if (preco != null) {
+			query.setParameter("preco", query);
+		}
+		if (dataCadastro != null) {
+			query.setParameter("dataCadastro", query);
+		}
+		return query.getResultList();
 
 	}
 
